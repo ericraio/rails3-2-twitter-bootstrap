b5eaff2796e1a50d5973bfa8b754b8a4f40b2a45
Eric Raio <ericraio@gmail.com>
2012-02-28 23:45:48 -0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayeAnime::Application.config.secret_token = 'daaf103b1b28fade88f31639a807b9dda16d9dd6f2c11893452531b552b68c381acca4104de92ffaa6c7524abee06e2ee43b05bf27baa3b8c246839c8cf854ed'
